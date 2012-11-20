fbf4e429693212120d7a9b84dd0c9cb84c48f49e
aurelien.pacaud@gmail.com <aurelien.pacaud@gmail.com@6d6a84e7-b24e-9fd6-a6ec-b75f3c18ea14>
2012-11-20 19:08:14 +0000

    Ajout de l'injecteur + refonte gestion de la page principale

--- trunk/GoogleDriveLight/src/org/isima/ui/utils/FileOperations.java
+++ trunk/GoogleDriveLight/src/org/isima/ui/utils/FileOperations.java
@@ -3,15 +3,12 @@ package org.isima.ui.utils;
 import java.io.File;
 import java.io.IOException;
 
-import javax.faces.context.FacesContext;
-import org.isima.ui.faces.bean.view.FileListBean;
-
 public class FileOperations {
 	
 	static public void createFile (String filename)
 	{
-		FileListBean flb = (FileListBean)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("fileListBean");
-		String path = flb.getCurrentDirectory();
+		
+		String path = "";
 		
 		File newFile = new File(path,filename);
 		try 
