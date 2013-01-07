d2cebda9830f9cb7c26162a463a79b48fec61f69
aurelien.pacaud@gmail.com <aurelien.pacaud@gmail.com@6d6a84e7-b24e-9fd6-a6ec-b75f3c18ea14>
2013-01-07 22:26:30 +0000

    Ajout d'un service pour la gestion du login

---
+++ trunk/GoogleDriveLight/src/org/isima/services/ILoginService.java
@@ -0,0 +1,6 @@
+package org.isima.services;
+
+public interface ILoginService {
+
+	public boolean accept(String login, String password);
+}
--- trunk/GoogleDriveLight/src/org/isima/ui/faces/bean/login/LoginManagedBean.java
+++ trunk/GoogleDriveLight/src/org/isima/ui/faces/bean/login/LoginManagedBean.java
@@ -2,22 +2,42 @@ package org.isima.ui.faces.bean.login;
 
 import java.io.Serializable;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
+import javax.annotation.PostConstruct;
 
+import org.isima.services.ILoginService;
+import org.isima.singleton.GoogleDriveLightInjector;
 import org.isima.ui.utils.MessageBundle;
+
+import fr.isima.annotation.Inject;
+import fr.isima.exception.MultipleBindException;
+import fr.isima.exception.NotNullBindingException;
+import fr.isima.injector.Injector;
 
 public class LoginManagedBean implements Serializable  {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private final String allowUsername = "admin";
-	private final String allwPassword = "admin";
-	
 	private String username;
 	private String password;
 	
 	private boolean connected = false;
+	
+	@Inject
+	private ILoginService loginService;
+	
+	@PostConstruct
+	public void init() {
+		
+		Injector injector = GoogleDriveLightInjector.getInstance();		
+		
+		try {
+			injector.inject(this);
+		} catch (NotNullBindingException e) {
+			e.printStackTrace();
+		} catch (MultipleBindException e) {
+			e.printStackTrace();
+		}	
+	}
 	
 	/***
 	 * Effectue la verification du login afin d'autoriser ou non l'utilisateur à se connecter.
@@ -26,16 +46,15 @@ public class LoginManagedBean implements Serializable  {
 	public String login() {
 		
 		/* Si toutes les informations sont bonnes, on autorise la connexion.*/
-		if(allowUsername.equals(username) && allwPassword.equals(password)) {
+		if(loginService.accept(username, password)) {
 			
-			connected = true;
+			connected = true;		
 			return "loginSuccess";
 		}
 		
 		connected = false;
 		
-		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(MessageBundle.getMessage("connectionNotAllowed")));		
+		MessageBundle.displayErrorMsg(MessageBundle.getMessage("connectionNotAllowed"));	
 		
 		return "loginFailed";
 	}
